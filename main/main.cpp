265da7ef5959fc42ece169e0bcc8a0d1e1403b41
Alan Martínez <alan.martinez5691@alumnos.udg.mx>
2020-09-14 19:52:11 -0500

    Capturar la edad e imprimimos la edad + 1

--- main/main.cpp
+++ main/main.cpp
@@ -4,7 +4,16 @@ using namespace std;
 
 int main()
 {
+    char nombre[100];
+    unsigned int edad;
+
     printf ("Hola mundo!\n");
+    printf ("Nombre: ");
+    fgets (nombre, sizeof(nombre), stdin);
+    printf ("Hola %s", nombre);
+    printf ("Edad: ");
+    scanf ("%u", &edad);
+    printf ("Va a cumplir %u", edad + 1);
 
     return 0;
 }
